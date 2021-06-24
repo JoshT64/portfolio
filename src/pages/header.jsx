ecf873e250056fd335cb4872fdca96fc76840be3
JT <joshuaharrison1731@gmail.com>
2021-06-24 14:35:54 -0400

    Add Resume info

---
+++ src/pages/header.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <h1 style={props.style}>Joshua Toney</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default Header;
